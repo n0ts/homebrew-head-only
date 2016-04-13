9febad72bee51fcbe9d9ebcff3cd3614c5a0e5d4
Baptiste Fontaine <batifon@yahoo.fr>
2016-04-13 22:06:44 +0200

    kerl: migrate to core
    Closes #206.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- kerl.rb
+++
@@ -1,9 +0,0 @@
-class Kerl < Formula
-  homepage "https://github.com/yrashk/kerl"
-  head "https://github.com/yrashk/kerl.git"
-
-  def install
-    bin.install "kerl"
-    bash_completion.install "bash_completion/kerl"
-  end
-end
