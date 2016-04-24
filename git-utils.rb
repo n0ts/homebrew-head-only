ed4a3c84048135e237c12a952b75bb89ba516f5e
Baptiste Fontaine <batifon@yahoo.fr>
2016-04-24 22:19:30 +0200

    git-utils: migrate to core
    Closes #207.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- git-utils.rb
+++
@@ -1,8 +0,0 @@
-class GitUtils < Formula
-  head "https://github.com/ddollar/git-utils.git"
-  homepage "https://github.com/ddollar/git-utils"
-
-  def install
-    bin.install Dir["git-*"]
-  end
-end
